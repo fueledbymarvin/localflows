7981d66d89e5cfc1fd46e4877ff7397c3049ac09
Harvey Xia <harvey@harveyxia.com>
2013-11-08 18:53:32 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Localflows::Application.config.secret_key_base = 'a13cc4f50e93e03191eed2dff0c7b3ad23185a7c72fa60f40e224d2abd2f16f7fd405a6a999486dfc8a92a42dc7b3f6fbc4b8b61dc72d2cf9707b711ddd3a53e'
