89ec53ec5ce94f80563b16eaf0ebc48e9e991333
Harvey Xia <harvey@harveyxia>
2013-11-08 21:11:37 -0500

    Create/save user from google callback hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ActiveRecord::Base
 
-    def self.from_omniauth(auth) do
-        
-        # where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
-        #   #stuff
-        #   user.save!
-        # end
+    def self.from_omniauth(auth)
+
+        user = find_or_initialize_by(gid: auth.extra.raw_info.uid)
+
+        user.name = auth.extra.raw_info.name
+        user.email = auth.extra.raw_info.email
+        user.image = auth.extra.raw_info.picture
+
+        user.gaccess = auth.credentials.token
+        user.grefresh = auth.credentials.refresh_token
+
+        user.save
     end
 end
