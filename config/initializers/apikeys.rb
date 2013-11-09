8209c5c4d76f3dab42edcf85a27c791b47629174
Harvey Xia <harvey@harveyxia>
2013-11-08 20:28:24 -0500

    fixed oauth route bug

--- config/initializers/apikeys.rb
+++ config/initializers/apikeys.rb
@@ -1,5 +1,5 @@
 ENV['google_key'] = "965729483714" 
-ENV['google_secret'] = "c59bd62b1863bf443cae32ba3306151c"
+ENV['google_secret'] = "ZZNZzAy7YP4_oRax39pMqGcX"
 ENV['eventful_key'] = "QKfkq5gSS25fkGPQ"
 ENV['groupme_key'] = "pUmGFA95Duyn8sbsOqneyxbTl0iQuRLbiNbNN4GCeb8oQAZ8"
 
