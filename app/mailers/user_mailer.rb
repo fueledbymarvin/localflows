14eb76e58a2e0907dff2dcc9e52e203fd69fe6fa
Harvey Xia <harvey@harveyxia.com>
2013-11-09 17:01:52 -0500

    Added event time to email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ActionMailer::Base
     default from: "hxi33x@gmail.com"
 
-    def event_confirmation(current_user, event_title, event_descripton, email)
+    def event_confirmation(current_user, event_title, event_description, email)
         @user = current_user
         @event_title = event_title
-        @event_descripton = event_descripton
+        @event_description = event_description
         mail(to: email, subject: "You've been invited to an event!")
     end
 end
