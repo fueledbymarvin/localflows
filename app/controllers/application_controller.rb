a260c1ce45135d95463756706b32af7f2a60c3e4
Marvin Qian <fueledbymarvin@gmail.com>
2013-11-08 19:52:19 -0500

    Setup routes and controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,27 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  private
+  def current_user
+  	@current_user ||= Member.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authenticated
+  	if current_user.nil?
+      session[:source] = request.path
+  		flash[:error] = "You must be signed in to do that!"
+  		redirect_to :root
+  	end
+  end
+
+  def admin
+  	if current_user.nil? || !current_user.admin
+  		session[:source] = request.path
+  		flash[:error] = "You must be signed in to do that!"
+  		redirect_to :root
+  	end
+  end
 end
---
+++ test/helpers/flows_controller_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class FlowsControllerHelperTest < ActionView::TestCase
+end
