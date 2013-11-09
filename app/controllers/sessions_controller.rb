bd2c1cd4b33369e9c508b182517116c631aa8884
Harvey Xia <harvey@harveyxia.com>
2013-11-08 20:28:24 -0500

    fixed oauth route bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,15 @@
 class SessionsController < ApplicationController
-  def create
-  end
+    def create
+        raise env["omniauth.auth"].to_yaml
+        # user = User.from_omniauth(env["omniauth.auth"])
+        # session[:user_id] = user.id
+        # source = session[:source] || :root
+        # session[:source] = nil
+        # redirect_to source, :notice => "Signed in!"
+    end
 
-  def destroy
-  end
+    def destroy
+        reset_session
+        redirect_to :root, :notice => "Signed out!"
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+
+    def self.from_omniauth(auth) do
+        
+        # where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+        #   #stuff
+        #   user.save!
+        # end
+    end
 end
