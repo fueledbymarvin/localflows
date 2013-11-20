c6ce5ae4e8d1a56e06581d2f9835cd64879a23ef
Harvey Xia <harvey@harveyxia.com>
2013-11-20 17:55:39 -0500

    Added my Google API key

--- config/initializers/apikeys.rb
+++ config/initializers/apikeys.rb
@@ -1,5 +1,11 @@
-ENV['google_key'] = "965729483714" 
-ENV['google_secret'] = "ZZNZzAy7YP4_oRax39pMqGcX"
+# Marvin's key
+# ENV['google_key'] = "965729483714" 
+# ENV['google_secret'] = "ZZNZzAy7YP4_oRax39pMqGcX"
+
+# Harvey's key
+ENV['google_key'] = "297702143266" 
+ENV['google_secret'] = "siHSCSmJT6UxOdgk7LKd9V1p"
+
 ENV['eventful_key'] = "QKfkq5gSS25fkGPQ"
 ENV['groupme_key'] = "pUmGFA95Duyn8sbsOqneyxbTl0iQuRLbiNbNN4GCeb8oQAZ8"
 ENV['google_maps_key'] = "AIzaSyClGdFH3vqDKCousZQS8vr3O7b7fZX4POk"
@@ -13,8 +19,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
       image_aspect_ratio: "square",
       image_size: 60,
       access_type: "offline",
-      redirect_uri:'http://localhost:3000/auth/google/callback'
-      # redirect_uri:'http://guarded-sands-6345.herokuapp.com/'
+      # redirect_uri:'http://localhost:3000/auth/google/callback'
+      redirect_uri:'http://guarded-sands-6345.herokuapp.com/auth/google/callback'
     }
 	#client_options: { :ssl => { :ca_file => '/usr/lib/ssl/certs/ca-certificates.crt' } }
 end
