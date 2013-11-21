b0f01fd98d20b2cd499bb2559098909fc3b3009e
Harvey Xia <harvey@harveyxia.com>
2013-11-21 03:03:28 -0500

    bug fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Localflows::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  GA.traker = "UA-25655664-3"
+  GA.tracker = "UA-25655664-3"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
